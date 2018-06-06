77acfe692a6af1ca6a14a6bfbb2d187606dfbb3a
Marcin Trendota <marcin@trendota.com>
2018-06-06 02:21:14 +0200

    Working stub for iOS

---
+++ ios/xcode/multiplatformtest/RepositoryListViewController.h
@@ -0,0 +1,14 @@
+//
+//  RepositoryListViewController.h
+//  multiplatformtest
+//
+//  Created by Marcin Trendota on 11.05.2018.
+//  Copyright © 2018 MoonWolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RepositoryListViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *repositoryTableView;
+
+@end
